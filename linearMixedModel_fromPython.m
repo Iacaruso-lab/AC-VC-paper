9a79278a5f01556867666a111fad2eed8b614fbe
AlexEgeaWeiss <alexander.egea-weiss@crick.ac.uk>
2025-07-24 13:24:33 +0100

    code cleanup

--- linearMixedModel_fromPython.m
+++ linearMixedModel_fromPython.m
@@ -1,23 +1,9 @@
 function [res, fitLines, fitCI] = linearMixedModel_fromPython(df_path, formula,outputPath)
 
-% df_path = 'Z:\\home\\shared\\Alex_analysis_camp\\FV_dataset_all\\analysisOutputs\\df_bySession_green_freq_forLMM.csv';
-% formula = ['peakElev_bySession ~ elev + (1|animal)'];  
-% % df_path = 'Z:\\home\\shared\\Alex_analysis_camp\\CS_dataset_all\\analysisOutputs\\df_forTest.csv'
-% 
 df = readtable(df_path);
 
-% df(ismember(df.area,'OUT'),:)=[];
-% df(ismember(df.area,'AL'),:)=[];
-% df(ismember(df.area,'LM'),:)=[];
-% df(ismember(df.area,'P'),:)=[];
-% df(ismember(df.area,'POR'),:)=[];
-% df(ismember(df.area,'LI'),:)=[];
-% df(ismember(df.area,'PM'),:)=[];
-% df(ismember(df.area,'AM'),:)=[];
-
-model1 = fitlme(df, formula) %% area as a random effect
+model1 = fitlme(df, formula) 
 res0 = model1.Coefficients;
-% anova(model1)
 
 for i=1:height(res0)
     name = res0.Name{i};
@@ -53,31 +39,6 @@ end
 
 save(outputPath, 'res');
 
-% tblnew = table();
-% tblnew.y = linspace(min(df.y),max(df.y), height(df))';
-% tblnew.x = zeros(height(df),1);
-% % tblnew.x = linspace(min(df.x),max(df.x),height(df))';
-% tblnew.animal =zeros(height(df),1);
-% tblnew.sessionIdx = zeros(height(df),1);
-% % [~,~,rEffects] = randomEffects(model1);
-% 
-% 
-% [ypred,yCI,DF] = predict(model1,tblnew);
-% 
-% figure(); 
-% h1 = line(tblnew.y,ypred);
-% hold on;
-% h2 = plot(tblnew.y,yCI,'g-.');hold on
-% scatter(df.y, df.spline_peak)
-% fitLine = res0.Estimate(1) + res0.Estimate(3)*tblnew.y
-% h3 = line(tblnew.y,fitLine, color='k');
-% 
-% 
-% figure(); 
-% h1 = line(tblnew.x,ypred);
-% hold on;
-% h2 = plot(tblnew.x,yCI,'g-.');hold on
-% scatter(df.x, df.spline_peak)
 
 
 
